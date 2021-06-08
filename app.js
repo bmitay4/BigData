477a8a655432664f90450f25664ed88037588bf0
Itay <33125525+bmitay4@users.noreply.github.com>
2021-06-09 00:47:49 +0300

    upload dashboard

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.static('public'))
+app.set('view engine', 'ejs')
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    var cards=["Borrowed","Annual Profit","Lead Conversion","Average Income",];
+  res.render("./pages/index",{cards:cards});
 })
 
 app.listen(process.env.PORT || 3000, function(){
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-  });
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
