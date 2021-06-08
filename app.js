4ab201d7ad8b229d9fd8293eb3721d1a4b117c25
Itay <33125525+bmitay4@users.noreply.github.com>
2021-06-09 01:06:13 +0300

    edit dashborad, add github link

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
 
-//For local running, disable the above lines
+// // For local running, disable the above lines
 // app.listen(port, () => {
 //   console.log(`Example app listening at http://localhost:${port}`)
 // })
