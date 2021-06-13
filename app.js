b3d4962d3ea23e3be5beea4ba8a3625a6d7a1b2d
Itay <33125525+bmitay4@users.noreply.github.com>
2021-06-13 14:42:50 +0300

    Add code that allows writing to Mongod DB Server

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require('express')
+const MongoClient = require('mongodb').MongoClient;
+
+// MongoDB Details
+const uri = "mongodb+srv://dsi:ariel2021@cluster0.lkuyv.mongodb.net/vehicles?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
 const app = express()
 const port = 3000
+var myobj = { name: "rina cohen", address: "TLV" }
+
 
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
@@ -10,11 +18,23 @@ app.get('/', (req, res) => {
   res.render("./pages/index",{cards:cards});
 })
 
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+// Adding a single document to mongoDB
+client.connect(err => {
+  const collection = client.db("vehicles").collection("section_2");
+  // perform actions on the collection object
+  collection.insertOne(myobj, function(err, res) {
+    if (err) throw err;
+  console.log("1 document inserted");
+  client.close();
+  });
 });
 
-// // For local running, disable the above lines
-// app.listen(port, () => {
-//   console.log(`Example app listening at http://localhost:${port}`)
-// })
+// // For heroku running
+// app.listen(process.env.PORT || 3000, function(){
+//   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+// });
+
+// For local running
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
